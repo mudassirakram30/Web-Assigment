526c9faead61152ddfa69ae24e3a69ecb2a633d5
Mudassir Shaikh <mudassirakram30@gmail.com>
2026-01-22 23:12:19 +0500

    add some functions

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ questions.forEach(function (question) {
     }
   });
 });
+
+function changeImage() {
+  var img = document.getElementById('hoverimage');
+  img.src = './All images/trade image 2.png';
+}
+function beforeImg() {
+    var img = document.getElementById('hoverimage');
+    img.src = './All images/herosection img.png';
+  }
+  var pass = document.getElementById("password");
+var checkbox = document.getElementById("showPass");
+
+checkbox.addEventListener("change", function() {
+    if (this.checked) {
+        pass.type = "text"; 
+    } else {
+        pass.type = "password";
+    }
+});
